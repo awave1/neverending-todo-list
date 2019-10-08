6002b5bb124973fad7cd0d02575fdad2cebce5d3
Artem Golovin <artgolovin96@gmail.com>
2019-10-08 16:22:36 -0600

    Initial Commit

---
+++ neverending-todo-list/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  neverending-todo-list
+//
+//  Created by awave on 2019-10-08.
+//  Copyright © 2019 awave. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
